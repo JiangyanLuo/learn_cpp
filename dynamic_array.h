9ad52691fb34de9ba1fa8e81f26af0c51f02dcca
Jiangyan Luo <catcat709@gmail.com>
2023-04-13 10:13:45 -0400

    templated dynamic array

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,23 +1,138 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cassert>
 
+#define max(a,b) a > b ? a : b
+using std::cout;
+
+template <typename T>
 class Dynamic_array {
     int len;
     int capacity;
-    int* data;
+    T* data;
 public:
 
-    Dynamic_array(int initial_capacity = 0);
+    Dynamic_array(int initial_capacity = 0): len(0), capacity(initial_capacity) {
+        if(initial_capacity < 1) {
+            data = nullptr;
+        } else {
+            // data = (T*) malloc(initial_capacity * sizeof(T));
+            data = new T[initial_capacity];
+        }
+    };
 
-    ~Dynamic_array();
+    ~Dynamic_array(){
+        // if(data != nullptr) {
+        //     free(data);
+        // }
 
-    void append(int value);
-    void pop();
-    void sort();
-    void clear();
-    void set(int index, int value);
-    int get(int index);
-    void print();
+        // delete is safe for nullptr, so we don't need to check if it is nullptr. 
+        //and we allocated multiple T using [], so we need to delete all of them using delete[].
+        delete[] data; 
+    };
 
-    static Dynamic_array* create(int initial_capacity = 0);
-    static void destroy(Dynamic_array*);
+    // void append(T value){
+    //     int newlen = len + 1;
+    //     if(newlen > capacity) {
+    //         int newcapacity = max(1,capacity * 2);
+    //         T* newdata =(T*) malloc(sizeof(T) * newcapacity);
+    //         if(data != nullptr) {
+    //             memcpy(newdata,data,len * sizeof(T));
+    //             free(data);
+    //         }
+    //         data = newdata;
+    //         capacity = newcapacity;
+    //     }
+    //     len = newlen;
+    //     data[len-1] = value;
+    // };
+
+    template<typename...Args>
+    T& emplace(Args&&...args){ // && is rvalue reference
+        int newlen = len + 1;
+        if(newlen > capacity) {
+            int newcapacity = max(1,capacity * 2);
+            //T* newdata =(T*) malloc(sizeof(T) * newcapacity);  
+            //malloc doesn't call constructor, new does.
+            T* newdata = new T[newcapacity]; // new T[n] will use default constructor and allocate n times of T size.
+            assert(newdata);
+            if(data != nullptr) {
+                // memcpy(newdata,data,len * sizeof(T));
+                // free(data);
+                for(int i = 0; i < len; i++){
+                    newdata[i] = std::move(data[i]); // force to use move constructor. move constructor moves ownership of the data.
+                }
+                // newdata = std::move(data);
+                delete[] data;
+            }
+            data = newdata;
+            capacity = newcapacity;
+        }
+        len = newlen;
+        return data[len-1] = T{std::forward<Args>(args)...}; // perfect forwarding
+    };
+
+    void pop() {
+        if(len < 1) {
+            cout << "Empty array.";
+        } else {
+            len--;
+        }
+
+    }
+
+    void sort() {
+        int flag;
+        do {
+            flag = 0;
+            for(int i = 0; i < len - 1; i++) {
+                if(data[i] > data[i+1]){
+                    T temp = data[i+1];
+                    data[i+1] = data[i];
+                    data[i] = temp;
+                    flag = 1;
+                }
+            }
+
+        } while (flag);
+        
+    }
+
+    void clear() {
+        len = 0;
+    }
+
+    void set(int index,T value) {
+        if(len > index) {
+            data[index] = value;
+        } else {
+            cout << "out of bounds.";
+        }
+    }
+
+    T get(int index) {
+        if(len <= index) {
+            cout << "out of bounds.";
+        }
+        return data[index];
+    }
+
+    void print() {
+        if(len < 1) {
+            cout << "empty array.";
+        } 
+        for(int i = 0; i < len; i++) {
+            cout << data[i] << " ";
+        }
+
+        cout << '\n';
+
+    }
+
 };
+
+
+
+
 
--- dynamic_array_main.cpp
+++ dynamic_array_main.cpp
@@ -1,30 +1,112 @@
 #include <iostream>
+#include <string>
 #include "dynamic_array.h"
 
 
 using std::cout;
 
+struct Plant {
+    std::string name;
+    int age;
 
-int main() {
+    Plant (const char* name_ = "", int age_ = 0): name(name_), age(age_) {}
 
-    Dynamic_array arr;
+    bool operator > (const Plant& other) const{
+        return name > other.name;
+    }
+
+    operator std::string () const { //to string
+        return name + ":" + std::to_string(age);
+    }
+
+    // // Plant(Plant&&) = delete; // this is delete the move constructor, so this class can be copied , can't be moved
+    // Plant(Plant&& other) { // override the move constructor to do a copy
+    //     name = other.name;
+    //     age = other.age;
+    // }
+
+    // Plant(const Plant& other) { //copy constructor
+    //     name = other.name;
+    //     age = other.age;
+    // }
+
+    // Plant& operator = (const Plant& other) { //override assignment operator
+    //     name = other.name;
+    //     age = other.age;
+
+    //     return *this;     
+    // }
+
+
+};
+
+std::ostream& operator << (std::ostream& lhs, Plant& rhs) {
     
-    arr.append(1);
-    arr.append(0);
-    arr.append(3);
-    arr.append(2);
-    arr.print();
+    lhs << (std::string)rhs; //std::string str = rhs;
 
-    arr.sort();
-    arr.print();
+    return lhs;
+}
+ 
+int main() {
+    
 
-    arr.set(3,11);
-    arr.print();
-    arr.pop();
-    arr.print();
+    // Dynamic_array<int> arr;
+    
+    // arr.append(1);
+    // arr.append(0);
+    // arr.append(3);
+    // arr.append(2);
+    // arr.print();
 
-    arr.clear();
-    arr.print();
+    // arr.sort();
+    // arr.print();
+
+    // arr.set(3,11);
+    // arr.print();
+    // arr.pop();
+    // arr.print();
+
+    // arr.clear();
+    // arr.print();
+
+    // Dynamic_array<std::string> arr;
+    
+    // arr.append("hello");
+    // arr.append("apple");
+    // arr.append("banana");
+    // arr.append("cat");
+    // arr.print();
+
+    // arr.sort();
+    // arr.print();
+
+    // arr.set(3,"popo");
+    // arr.print();
+    // arr.pop();
+    // arr.print();
+
+    // arr.clear();
+    // arr.print();
+
+
+    Dynamic_array<Plant> plants;
+    
+    plants.emplace("orange", 1);
+    plants.emplace("apple", 2);
+    plants.emplace("banana");
+    plants.emplace("cat");
+    plants.print();
+
+    plants.sort();
+    plants.print();
+
+    plants.set(3,"popo");
+    plants.print();
+    plants.pop();
+    plants.print();
+
+    plants.clear();
+    plants.print();
 
     return 0;
 }
