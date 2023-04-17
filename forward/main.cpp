ce80ac411073c76183fead86aa2379b8cf5de928
Jiangyan Luo <catcat709@gmail.com>
2023-04-17 13:37:56 -0400

    cast

--- forward/main.cpp
+++ forward/main.cpp
@@ -2,7 +2,13 @@
 
 using std::cout;
 
+struct yo {
+    bool bbb;
+};
+
 struct Animal{
+    int a = 5;
+
     Animal(){
         cout << "Animal constructor\n";
     }
@@ -13,7 +19,7 @@ struct Animal{
     Animal(Animal&& other){
         cout << "Animal move constructor\n";
     }
-    ~Animal(){
+    virtual ~Animal(){
         cout << "Animal destructor\n";
     }
 
@@ -27,9 +33,15 @@ struct Animal{
         return *this;
     }
 
+    void print() {
+        cout << a << '\n';
+    }
+
 };
 
 struct Dog: Animal{
+    std::string str {"doggg"};
+
     Dog(){
         cout << "Dog constructor\n";
     }
@@ -57,6 +69,30 @@ struct Dog: Animal{
         return *this;
     }
 
+    // operator std::string () const {
+    //     return str;
+    // }
+
+    void print() {
+        cout << str << '\n';
+    }
+
+
+};
+
+struct Cat: Animal{
+    float b {20.5};
+
+    Cat(){
+        cout << "cat constructor\n";
+    }
+    ~Cat(){
+        cout << "cat destructor\n";
+    }
+
+    void print() {
+        cout << b << '\n';
+    }
 
 };
 
@@ -78,6 +114,16 @@ void other_fun(T&& t) {
     func(std::forward<T>(t));
 }
 
+void patate(Animal* animal) {
+    auto stuff = dynamic_cast<Cat*>(animal);
+    if (stuff) {
+        stuff->print();
+    } else {
+
+    }
+
+}
+
 int main() {
     // Dog dog;
     // Dog otherdog = dog;
@@ -87,10 +133,23 @@ int main() {
     // thirddog = otherdog;
     // cout << "-----------\n";
 
-    int a{5};
-    const int b{5};
-    other_fun(a);
-    other_fun(b);
-    other_fun(5);
+    // int a{5};
+    // const int b{5};
     // other_fun(a);
+    // other_fun(b);
+    // other_fun(5);
+  
+
+
+    Dog dog;
+    // Cat* cat = new Cat;
+    // Animal* animal = new Dog;
+    // animal = cat;
+
+    // static cast : most common cast,will try to automatically use overloaded cast operators if present.
+    // dynamic cast : only works with object pointers, when the object is polymorphic (has a virtual member)
+                    // don't forget to check if it is nullptr.
+    // reintepret cast : only when we know what we are doing, will not convert the data in any way.
+    // (T) : old style c cast, in c++, compiler will choose one of 3 casts above for us at compile time
+
 }
