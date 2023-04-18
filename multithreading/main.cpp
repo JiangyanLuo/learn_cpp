7b1218261e92bc9703707e8e835227b4d5ff846d
Jiangyan Luo <catcat709@gmail.com>
2023-04-18 11:50:31 -0400

    mutex and recursive mutex

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -1,11 +1,118 @@
 #include <iostream>
-#include <future>
+#include <mutex>
+#include <atomic>
+#include <cassert>
 
 #include <thread>
 #include <chrono>
 
+struct Mutex {
+    std::atomic<bool> mu {false};
+    void lock(){
+        while(!try_lock());
+    }
+    bool try_lock(){
+        return !mu.exchange(true);
+    }
+    void unlock(){
+        mu.store(false);
+    }
+};
+
+struct RecursiveMutex {
+    std::atomic<bool> mu {false};
+    std::thread::id tid {};
+    std::atomic<int> count{0};
+    void lock(){
+        while(!try_lock());
+    }
+    bool try_lock(){
+        if (!mu.exchange(true)) {
+            tid = std::this_thread::get_id();
+            count++;
+            return true;
+        } else if (tid == std::this_thread::get_id()) {
+            count++;
+            return true;
+        } else return false;
+    }
+    void unlock(){
+        assert(tid == std::this_thread::get_id());
+        assert(count > 0);
+        count--;
+        if (count == 0) {
+            tid = {};
+            mu.store(false);
+        }
+    }
+};
+
+template<class T>
+struct LockGuard{
+    T* mu;
+    LockGuard(T& mu_) : mu(&mu_){
+        mu->lock();
+    }
+    LockGuard(LockGuard&& other){
+        mu = other.mu;
+        other.mu = nullptr;
+    }
+    LockGuard(const LockGuard&) = delete;
+    ~LockGuard(){
+        if(mu) mu->unlock();
+    }
+};
+
 #define SLEEP(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 std::thread td1;
+
+bool active {true};
+int counter {0};
+
+RecursiveMutex mu;
+
+struct Numbers {
+    int a {};
+    int b {};
+    int c {};
+    int d {};
+
+    Numbers() = default;
+
+    Numbers(const Numbers& other) {
+        LockGuard<RecursiveMutex> lkg{mu};
+        a = other.a;
+        b = other.b;
+        c = other.c;      
+    }
+
+    void dd() {
+        LockGuard<RecursiveMutex> lkg{mu};
+        d = c * 2;
+    }
+
+} numbers;
+
+void producer(){
+    while(active){
+        LockGuard<RecursiveMutex> lockgd{mu};
+        numbers.a++;
+        numbers.b = numbers.a + 1;
+        numbers.c = numbers.b + 1;
+        numbers.dd();
+    }
+
+}
+
+void consumer() {
+    while(active){
+        auto othernumbers = numbers;
+        if(othernumbers.b != othernumbers.a + 1 || othernumbers.c != othernumbers.b + 1) {
+            counter ++;
+            std::cout << "OOPS\n" << std::flush;
+        }
+    }
+}
 
 void func(){
     std::cout << "first or second?\n" << std::flush;
@@ -14,7 +121,14 @@ void func(){
 }
 
 int main() {
-    td1 = std::thread{func};
-    std::cout << "should print first\n" << std::flush;
+    td1 = std::thread{producer};
+    std::thread td2 {consumer};
+    SLEEP(2000)
+
+    active = false;
+
     if(td1.joinable()) td1.join();
+    if(td2.joinable()) td2.join();
+
+    std::cout << counter << '\n';
 }
